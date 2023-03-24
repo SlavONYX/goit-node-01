f6942f3bc69456831a073f4cf21561ed13468fd1
SlavONYX <slavonixxx@gmail.com>
2023-03-24 23:15:34 +0200

    upd

---
+++ contacts.js
@@ -0,0 +1,54 @@
+const fs = require("fs").promises;
+const path = require("path");
+
+const { v4: uuidv4 } = require("uuid");
+const contactsPath = path.resolve("./db/contacts.json");
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(data);
+  return contacts;
+}
+
+async function getContactById(id) {
+  const contacts = await listContacts();
+  const result = contacts.find((item) => item.id === id);
+  if (!result) {
+    return null;
+  }
+  return result;
+}
+
+async function removeContact(id) {
+  const contacts = await listContacts();
+
+  const idx = contacts.findIndex((item) => item.id === id);
+  if (idx === -1) {
+    return null;
+  }
+  const [removeById] = contacts.splice(idx, 1);
+
+  async function updatedContacts(contacts) {
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  }
+
+  await updatedContacts(contacts);
+
+  return removeById;
+}
+
+async function addContact({ name, email, phone }) {
+  const contacts = await listContacts();
+  const newContact = { ...{ name, email, phone }, id: uuidv4() };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return newContact;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  removeContact,
+};
